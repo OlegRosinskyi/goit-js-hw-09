3366bd53d1d6aa900dc23cf57c9ade95678dac76
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-12-07 18:18:58 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,63 +1,71 @@
 const formEl = document.querySelector('.form');
+const buttonEl = document.querySelector('button');
+//console.log(buttonEl);
 const dataForm = {
-  delay: (delay = 2000),
-  step: (step = 1000),
-  amount: (amount = 3),
+  delay: (delay = 0),
+  step: (step = 0),
+  amount: (amount = 0),
 };
 const resultPromis = [];
 
 function createPromise(position, delay) {
   setTimeout(() => {
     const shouldResolve = Math.random() > 0.3;
-    console.log(shouldResolve);
-    console.log(position);
+    //console.log(shouldResolve);
 
     const promise = new Promise((resolve, reject) => {
       if (shouldResolve) {
         //resolve(`✅ Fulfilled promise ${position} in ${delay}ms`); // Fulfill
-
         //resolve(position)(delay);
-        resolve(delay, position);
+        resolve({ position: position, delay: delay });
       } else {
         //reject(`❌ Rejected promise ${position} in ${delay}ms`); // Reject
         // resolve(position);
-        resolve(delay, position);
+        resolve({ position: position, delay: delay });
       }
     });
     promise.then(
       result => {
         console.log(result);
-        resultPromis[position - 1].position = result;
-        resultPromis[position - 1].delay = result;
+        resultPromis[position - 1] = result;
+        //resultPromis[position - 1].delay = result;
         // alert(`✅ "${result}"`);
         console.log(resultPromis);
       },
       error => {
         console.log(error);
         // alert(`❌ "${error}"`);
-        resultPromis[position - 1].position = error;
-        resultPromis[position - 1].delay = error;
+        resultPromis[position - 1] = error;
+        //resultPromis[position - 1].delay = error;
         console.log(resultPromis);
       }
     );
   }, delay);
 }
 
-for (let i = 1; i < dataForm.amount + 1; i += 1) {
-  resultPromis.push({});
-  createPromise(i, delay + dataForm.step * i);
-}
+const onSubmit = event => {
+  event.preventDefault();
+  console.log(event.type);
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  if (delay.value === '' || step.value === '' || amount.value === '') {
+    return alert('Заповнить всі поля форми>');
+  }
 
-//const timerId = setInterval(callback, delay, arg1, arg2, ...);
-//-----------------------------------------------------------------------------
-//const isSuccess = true;
+  // console.log(`email: ${email.value}, Password: ${password.value}`);
 
-//const promise = new Promise((resolve, reject) => {
-//  setTimeout(() => {
-//    if (isSuccess) {
-//      resolve('Success! Value passed to resolve function');
-//    } else {
-//      reject('Error! Error passed to reject function');
-//    }
-//  }, 2000);
-//});
+  dataForm.delay = delay.valueAsNumber;
+  dataForm.step = step.valueAsNumber;
+  dataForm.amount = amount.valueAsNumber;
+  console.log(dataForm);
+
+  for (let i = 1; i < dataForm.amount + 1; i += 1) {
+    resultPromis.push({});
+    createPromise(i, dataForm.delay + dataForm.step * i);
+  }
+  event.currentTarget.reset();
+};
+
+//formEl.addEventListener('input', inputForm);
+formEl.addEventListener('submit', onSubmit);
