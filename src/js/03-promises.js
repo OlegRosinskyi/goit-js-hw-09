debd8f389f36923c0e35886a8798750dc366755e
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-12-07 18:41:49 +0200

    update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,7 +1,7 @@
 const formEl = document.querySelector('.form');
 const buttonEl = document.querySelector('button');
 //console.log(buttonEl);
-const dataForm = {
+let dataForm = {
   delayF: (delay = 0),
   stepF: (step = 0),
   amountF: (amount = 0),
